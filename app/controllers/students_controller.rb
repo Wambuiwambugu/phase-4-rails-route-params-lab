e224d2b06890d0075c4da553cc51096537344e8f
Maryanne Wambugu <2wambugu.marie@gmail.com>
2023-08-14 20:27:50 +0300

    done

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,8 +1,19 @@
 class StudentsController < ApplicationController
 
   def index
+    if params[:first_name]
+      students = Student.find(params[:first_name])
+    elseif params[:last_name]
+    students = Student.find(params[:last_name])
+    else 
     students = Student.all
+    end
     render json: students
+  end
+
+  def id
+    student = Student.find(params[:id])
+    render json: student
   end
 
 end
